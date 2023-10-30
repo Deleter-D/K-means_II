b862dff8d32741a5c5e08484f10dd366682f52d4
Deleter-D <867909454@qq.com>
2023-10-30 18:21:34 +0800

    update

--- k-means_II/include/kmeans.h
+++ k-means_II/include/kmeans.h
@@ -3,17 +3,27 @@
 class kmeans
 {
 public:
-    kmeans(float *original_data, size_t original_size, size_t k)
-        : original_data(original_data), original_size(original_size), k(k) {}
+    kmeans(float *original_data, size_t original_size, size_t original_dim, size_t k)
+        : original_data(original_data),
+          original_size(original_size),
+          original_dim(original_dim),
+          k(k) {}
 
     void init();      // 初始化过程
     void iteration(); // 迭代过程
 
 private:
+    float euclideanDistance(float *x, float *y, const int dim);                                   // 欧式距离
+    float costFromV2S(float *x, float *set, const int dim, const int size);                       // 向量与集合之间的代价
+    float costFromS2S(float *set1, float *set2, const int dim, const int size1, const int size2); // 集合与集合之间的代价
+
+private:
     float *original_data; // 原始向量集
     size_t original_size; // 原始向量个数
+    size_t original_dim;  // 原始向量维度
     float *center;        // 聚类中心集
-    size_t k;             // 聚类中心个数
+    size_t current_k;     // 聚类中心的当前个数
+    size_t k;             // 聚类中心的目标个数
 
     // 超参数
     float l;       // 超采样因子
--- k-means_II/src/kmeans.cpp
+++ k-means_II/src/kmeans.cpp
@@ -1,3 +1,5 @@
+#include <math.h>
+#include <cstring>
 #include <random>
 
 #include "../include/kmeans.h"
@@ -9,7 +11,47 @@ void kmeans::init()
     std::uniform_int_distribution<> distrib(0, original_size - 1);
 
     size_t index = distrib(gen);
-    
+
+    // 初始状态分配k个聚类中心的内存
+    size_t center_bytes = k * original_dim * sizeof(float);
+    center = (float *)malloc(center_bytes);
+    // 将第一个聚类中心并入集合C
+    memcpy(center, &original_data[index], original_dim * sizeof(float));
+
+    float phi = costFromS2S(original_data, center, original_dim, original_size, current_k);
 }
 
 void kmeans::iteration() {}
+
+float kmeans::euclideanDistance(float *x, float *y, const int dim)
+{
+    float sum = 0.0f;
+    for (int i = 0; i < dim; i++)
+    {
+        sum += (x[i] - y[i]) * (x[i] - y[i]);
+    }
+    return sum;
+}
+
+float kmeans::costFromV2S(float *x, float *set, const int dim, const int size)
+{
+    float min = MAXFLOAT;
+    float temp = MAXFLOAT;
+    for (int i = 0; i < size; i++)
+    {
+        temp = euclideanDistance(x, &set[i * dim], dim);
+        if (temp < min)
+            min = temp;
+    }
+    return min;
+}
+
+float kmeans::costFromS2S(float *set1, float *set2, const int dim, const int size1, const int size2)
+{
+    float sum = 0.0f;
+    for (int i = 0; i < size1; i++)
+    {
+        sum += costFromV2S(&set1[i * dim], set2, dim, size2);
+    }
+    return sum;
+}
