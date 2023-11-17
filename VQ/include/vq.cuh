4c30d1813f17fc80df779c59c682b70324e8be98
Deleter-D <867909454@qq.com>
2023-11-17 11:59:19 +0800

    update

--- VQ/include/vq.cuh
+++ VQ/include/vq.cuh
@@ -5,10 +5,10 @@
 #include "../../utils/include/common.cuh"
 #include "../../k-means/include/random_kmeans.h"
 
-void VQBuild(float *original_data, unsigned int original_size, int original_dim, char *prefix);
+extern "C" void VQBuild(float *original_data, unsigned int original_size, int original_dim, char *prefix);
 
 __global__ void levelQueryKernel(unsigned int *result, float *query_set, float *cluster, unsigned int *indices, unsigned int *belong,
                                  unsigned int dim, unsigned int query_size, unsigned int cluster_size, unsigned int indices_size);
-void VQQuery(unsigned int *result, float *query_set, unsigned int query_size,
-             float *original_data, unsigned int original_dim, unsigned int original_size, unsigned int topk,
-             float *cluster_0, float *cluster_1, unsigned int *indices_0, unsigned int *indices_1, unsigned int *catagories_count);
+extern "C" void VQQuery(unsigned int *result, float *query_set, unsigned int query_size,
+                        float *original_data, unsigned int original_dim, unsigned int original_size, unsigned int topk,
+                        float *cluster_0, float *cluster_1, unsigned int *indices_0, unsigned int *indices_1, unsigned int *catagories_count);
